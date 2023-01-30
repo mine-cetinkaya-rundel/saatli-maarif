9706456a2abcd6e068f75ee5dde204b678887d4d
mine-cetinkaya-rundel <cetinkaya.mine@gmail.com>
2023-01-29 20:07:39 -0800

    Update screen

---
+++ update_screen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+import sys
+import os
+
+from lib import epd7in5b_HD
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+
+try:
+    font24 = ImageFont.truetype(os.path.join('pic', 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join('pic', 'Font.ttc'), 18)
+
+    epd = epd7in5b_HD.EPD()
+
+    epd.init()
+    epd.Clear()
+
+    image = Image.open('saatli-maarif.bmp')
+    epd.display_black(epd.getbuffer(image))
+
+    epd.sleep()
+    
+    epd.Dev_exit()
+        
+except KeyboardInterrupt:    
+    epd7in5b_HD.epdconfig.module_exit()
+    exit()
